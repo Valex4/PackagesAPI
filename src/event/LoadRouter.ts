38e3668baa4540fc6a8ec9a32c246e83a0fe210f
Valex4 <yam778123@gmail.com>
2023-06-27 11:13:39 -0600

    [UPDATE] change LoadRouter to Consumer

--- src/event/LoadRouter.ts
+++ src/event/LoadRouter.ts
@@ -1,4 +1,4 @@
-import amqp from "amqplib";
+/*import amqp from "amqplib";
 import express from "express";
 
 const config = {
@@ -21,4 +21,37 @@ loadRouter.get("/", async function loadEvent(req, res) {
   await channel.close();
   await conn.close();
   res.status(200).send("OK");
-});
+});*/
+
+
+import amqp from "amqplib";
+import express from "express";
+
+export const loadRouter = express.Router();
+const rabbitSettings = {
+  protocol: "amqp",
+  hostname: "34.235.104.181",
+  port: 5672,
+  username: "guest",
+  password: "guest",
+};
+async function connect() {
+    const queue = "InitialEvent"
+    try {
+        const conn = await amqp.connect(rabbitSettings);
+        console.log('Conexión exitosa')
+        const channel = await conn.createChannel()
+        console.log ("Canal creado exitosamente")
+
+        channel.consume(queue, (msn:any)=> {
+            console.log(msn.content.toString())
+            channel.ack(msn)
+        })
+        
+
+    } catch (error) {
+        console.error('Error => ', error)    
+    }
+}
+
+connect();
